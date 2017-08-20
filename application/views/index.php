a4afc8fe87e2abdc7c805227665ec79818a078f3
Chivon <chhaichivon@2ntkh.com>
2017-08-20 15:40:21 +0700

    news-codeigniter

---
+++ application/views/index.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>News CodeIgniter</title>
+
+</head>
+<body>
+<div class="container">
+		<center>News CodeIgniter Web App</center>
+</div>
+</body>
+</html>
